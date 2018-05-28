ab4095da4c4f0359e913985f68d2d9bacca3c604
Benoît JOLLY <neb@ben.local>
2018-05-28 18:36:25 +0200

    Add css Style

--- meteo/src/store.js
+++ meteo/src/store.js
@@ -2,20 +2,18 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import moment from 'moment';
 import _ from 'lodash';
-// import { cloneDeep } from 'lodash/clone';
 import api from './api';
-
-// import _ from 'lodash';
 
 Vue.use(Vuex);
 
 const store = {
   state: {
-    count: 0,
+    loadingCurrent: false,
     currentWeather: {
       icon: null,
       temp: null,
     },
+    loadingForecast: false,
     forecastWeather: {
       firstDay: {
         d: null,
@@ -41,6 +39,9 @@ const store = {
     SET_CURRENT_WEATHER(state, payload) {
       state.currentWeather.icon = payload.weather[0].icon;
       state.currentWeather.temp = _.round(payload.main.temp);
+    },
+    SET_CURRENT_LOADING(state, payload) {
+      state.loadingCurrent = payload;
     },
     SET_FORECAST_WEATHER(state, payload) {
       const weekDayMin = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
@@ -68,51 +69,62 @@ const store = {
 
         arrayDataDays[j].tempMin.push(payload.list[i].main.temp_min);
         arrayDataDays[j].tempMax.push(payload.list[i].main.temp_max);
-        arrayDataDays[j].listIcon.push(payload.list[i].weather[0].icon);
+        arrayDataDays[j].listIcon.push(payload.list[i].weather[0].id);
       }
 
-      state.forecastWeather.firstDay.d = weekDayMin[arrayDataDays[0].weekDay];
-      state.forecastWeather.firstDay.tempMin = _.round(_.min(arrayDataDays[0].tempMin));
-      state.forecastWeather.firstDay.tempMax = _.round(_.max(arrayDataDays[0].tempMax));
-      state.forecastWeather.firstDay.icon = _.chain(arrayDataDays[0].listIcon).countBy().toPairs().maxBy(_.last)
+      state.forecastWeather.firstDay.d = weekDayMin[arrayDataDays[1].weekDay];
+      state.forecastWeather.firstDay.tempMin = _.round(_.min(arrayDataDays[1].tempMin));
+      state.forecastWeather.firstDay.tempMax = _.round(_.max(arrayDataDays[1].tempMax));
+      state.forecastWeather.firstDay.icon = _.chain(arrayDataDays[1].listIcon).countBy().toPairs().maxBy(_.last)
         .head()
         .value();
 
-      state.forecastWeather.secondDay.d = weekDayMin[arrayDataDays[1].weekDay];
-      state.forecastWeather.secondDay.tempMin = _.round(_.min(arrayDataDays[1].tempMin));
-      state.forecastWeather.secondDay.tempMax = _.round(_.max(arrayDataDays[1].tempMax));
-      state.forecastWeather.secondDay.icon = _.chain(arrayDataDays[1].listIcon).countBy().toPairs().maxBy(_.last)
+      state.forecastWeather.secondDay.d = weekDayMin[arrayDataDays[2].weekDay];
+      state.forecastWeather.secondDay.tempMin = _.round(_.min(arrayDataDays[2].tempMin));
+      state.forecastWeather.secondDay.tempMax = _.round(_.max(arrayDataDays[2].tempMax));
+      state.forecastWeather.secondDay.icon = _.chain(arrayDataDays[2].listIcon).countBy().toPairs().maxBy(_.last)
         .head()
         .value();
 
 
-      state.forecastWeather.thirdDay.d = weekDayMin[arrayDataDays[2].weekDay];
-      state.forecastWeather.thirdDay.tempMin = _.round(_.min(arrayDataDays[2].tempMin));
-      state.forecastWeather.thirdDay.tempMax = _.round(_.max(arrayDataDays[2].tempMax));
-      state.forecastWeather.thirdDay.icon = _.chain(arrayDataDays[2].listIcon).countBy().toPairs().maxBy(_.last)
+      state.forecastWeather.thirdDay.d = weekDayMin[arrayDataDays[3].weekDay];
+      state.forecastWeather.thirdDay.tempMin = _.round(_.min(arrayDataDays[3].tempMin));
+      state.forecastWeather.thirdDay.tempMax = _.round(_.max(arrayDataDays[3].tempMax));
+      state.forecastWeather.thirdDay.icon = _.chain(arrayDataDays[3].listIcon).countBy().toPairs().maxBy(_.last)
         .head()
         .value();
+    },
+    SET_FORECAST_LOADING(state, payload) {
+      state.loadingForecast = payload;
     },
   },
   actions: {
     getCurrentWeather: (context, payload) => {
+      context.commit('SET_CURRENT_LOADING', true);
       api.get(`https://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=e7223130fa39ca4cd87efbc3c352d534&units=metric`).then((response) => {
         context.commit('SET_CURRENT_WEATHER', response);
+        context.commit('SET_CURRENT_LOADING', false);
       }, (error) => {
         console.log(error.statusText);
+        context.commit('SET_CURRENT_LOADING', false);
       });
     },
     getForecastWeather: (context, payload) => {
+      context.commit('SET_FORECAST_LOADING', true);
       api.get(`https://api.openweathermap.org/data/2.5/forecast?q=${payload}&appid=e7223130fa39ca4cd87efbc3c352d534&units=metric`).then((response) => {
         context.commit('SET_FORECAST_WEATHER', response);
+        context.commit('SET_FORECAST_LOADING', false);
       }, (error) => {
         console.log(error.statusText);
+        context.commit('SET_FORECAST_LOADING', false);
       });
     },
   },
   getters: {
     currentWeather: state => state.currentWeather,
     forecastWeather: state => state.forecastWeather,
+    loadingCurrent: state => state.loadingCurrent,
+    loadingForecast: state => state.loadingForecast,
   },
 };
 
--- meteo/src/views/Home.vue
+++ meteo/src/views/Home.vue
@@ -1,16 +1,147 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    {{currentWeather}}
-    {{forecastWeather}}
-    {{cities}}
-    <button class="button counter__increment" @click="getWeather"><i class="fa fa-plus"></i></button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- {{currentWeather}}
+    {{forecastWeather}} -->
+    <label>Sélectonner votre ville</label>
+    <multiselect 
+      :options="cities"
+      v-model="selectedCity"
+      track-by="id"
+      label="nm"
+      @input="getWeather(selectedCity.nm)">
+    </multiselect>
+    <div v-if="selectedCity.nm !== null">
+      <div class="city">
+        <div class="title">
+          {{selectedCity.nm}}
+        </div>  
+      </div>
+      <div class="current">
+        <div class="info" v-if="!loadingCurrent">
+          <div class="logo">
+            {{currentWeather.icon}}
+          </div>
+          <div class="temp" >
+            {{currentWeather.temp}}°
+          </div>
+        </div>
+        <div class="spinner">
+          <spinner
+            :status="loadingCurrent"
+            :color="confSpinner.color"
+            :size="confSpinner.size"
+            :speed="confSpinner.speed">
+          </spinner>
+        </div>
+      </div>
+      <div class="forecast">
+        <div class="days" v-if="!loadingForecast">
+          <span>{{forecastWeather.firstDay.d}}</span>
+          <span>{{forecastWeather.secondDay.d}}</span>
+          <span>{{forecastWeather.thirdDay.d}}</span>
+        </div>
+        <div>
+          <div class="content"  v-if="!loadingForecast">
+            <div class="info-forecast">
+              <div class="logo">
+                {{forecastWeather.firstDay.icon}}
+              </div>
+              <div class="tempMax">
+                {{forecastWeather.firstDay.tempMax}}°
+              </div>
+              <div class="tempMin">
+                {{forecastWeather.firstDay.tempMin}}°
+              </div>
+            </div>
+            <div class="info-forecast">
+              <div class="logo">
+                {{forecastWeather.secondDay.icon}}
+              </div>
+              <div class="tempMax">
+                {{forecastWeather.secondDay.tempMax}}°
+              </div>
+              <div class="tempMin">
+                {{forecastWeather.secondDay.tempMin}}°
+              </div>
+            </div>
+            <div class="info-forecast">
+              <div class="logo">
+                <div class="wi-icon-800"></div>
+                {{forecastWeather.thirdDay.icon}}
+              </div>
+              <div class="tempMax">
+                {{forecastWeather.thirdDay.tempMax}}°
+              </div>
+              <div class="tempMin">
+                {{forecastWeather.thirdDay.tempMin}}°
+              </div>
+            </div>
+          </div>
+          <div class="spinner">
+            <spinner
+              :status="loadingForecast"
+              :color="confSpinner.color"
+              :size="confSpinner.size"
+              :speed="confSpinner.speed">
+            </spinner>
+          </div>
+        </div>
+      </div>
+    </div>   
   </div>
 </template>
 
+<style lang="scss">
+  .home {
+    max-width: 500px;
+    min-width: 50%;
+    display: flex;
+    flex-direction: column;
+    label {
+      display: flex;
+      justify-content: flex-start; 
+    }
+    .city {
+        padding: 20px;
+        .title{
+          background-color: #734DBA;
+          padding: 10px;
+          text-transform: uppercase;
+        }
+    }
+    .current {
+      .info {
+        .temp {
+          font-size: 30px;
+        }
+      }
+    }
+    .forecast {
+      padding: 20px;
+      .days{
+        background-color: #734DBA;
+        padding: 10px;
+        text-transform: uppercase;
+        display: flex;
+        justify-content: space-around;
+      }
+      .content {
+        display: flex;
+        justify-content: space-around;
+        padding: 10px;
+      }
+    }
+    .spinner {
+      display: flex;
+      justify-content: center;
+    }
+  }
+</style>
 <script>
 // @ is an alias to /src
+import Multiselect from 'vue-multiselect'
+import Spinner from 'vue-spinner-component/src/Spinner.vue'
 import HelloWorld from '@/components/HelloWorld.vue';
 import store from "../store";
 import cities from "../assets/static/cities-fr.js";
@@ -19,10 +150,18 @@ export default {
   name: 'home',
   components: {
     HelloWorld,
+    Multiselect,
+    Spinner,
   },
   data() {
     return {
       cities: cities,
+      selectedCity: {nm:'paris'},
+      confSpinner: {
+        speed: 0.5,
+        color: '9577CB',
+        size: 20,
+      }
     };
   },
   computed: {
@@ -34,14 +173,19 @@ export default {
     },
     forecastWeather() {
       return this.$store.getters.forecastWeather;
-    }
+    },
+    loadingCurrent() {
+      return this.$store.getters.loadingCurrent;
+    },
+    loadingForecast() {
+      return this.$store.getters.loadingForecast;
+    },
   },
   methods: {
     increment() {
       store.commit('increment');
     },
     getWeather(city) {
-      city = 'paris';
       this.$store.dispatch('getCurrentWeather',city);
       this.$store.dispatch('getForecastWeather',city);
     }
